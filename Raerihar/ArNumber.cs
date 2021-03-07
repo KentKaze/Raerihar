98026eeac49b7718818327fad2109adb0494dfa1
KentKaze <kentnewbie@hotmail.com>
2021-03-07 17:52:07 +0900

    Add project files.

---
+++ Raerihar/ArNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aritiafel.Organizations.RaeriharUniversity
+{
+    //ArNumber 架構
+
+    //1 Negative
+    //2 0 => 5  bit  Exponent
+    //2 1 => 13 bit  Exponent
+    //2 2 => 29 bit  Exponent
+    //2 3 => 61 bit  Exponnet
+    //N Exponent
+    //N Digits 二進位表示
+
+    //byte[]
+    public struct ArNumber //: IComparable, IComparable<ArNumber>, IConvertible, IEquatable<ArNumber>, IFormattable
+    {
+        private byte[] _Data;
+        public int Legnth { get; set; }
+        public void Test()
+        {
+            //BitConverter.
+        }
+        
+    }
+}
---
+++ RaeriharTest/MainTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aritiafel.Organizations.RaeriharUniversity;
+
+namespace RaeriharTest
+{
+    [TestClass]
+    public class MainTest
+    {
+        [TestMethod]
+        public void NewArNumber()
+        {
+            ArNumber a = 3;
+        }
+    }
+}
