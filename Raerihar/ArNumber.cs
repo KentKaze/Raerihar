ce66c15452828afa5d35d804090ac75ddd9cb187
KentKaze <kentnewbie@hotmail.com>
2021-03-11 21:45:13 +0900

    U

--- Raerihar/ArNumber.cs
+++ Raerihar/ArNumber.cs
@@ -18,31 +18,55 @@ namespace Aritiafel.Organizations.RaeriharUniversity
     //8	    	27			4
     //9	    	30			4
 
-    
-//		1 321974894 648943195.489415948 943195489  156
-//		- --------- --------- --------- ---------  ---
-//index	5		4			3		2			1	 0
-//bit	4		30		    30		30			30	10
-//bits = 134
-//bytes = 17
-//e = +18
+
+    //		1 321974894 648943195.489415948 943195489  156
+    //		- --------- --------- --------- ---------  ---
+    //index	5		4	 	3		  2        1        0
+    //bit	4		30	    30	     30	 	  30       10
+    //bits = 134
+    //bytes = 17
+    //e = +18  (18 + 1) % 9 = 1
+
+    //		1 321974894 648943195.489415948 943195489  156
+    //		- --------- --------- --------- ---------  ---
+    //index	5		4	 	3		  2        1        0
+    //bit	4		30	    30	     30	 	  30       10
+    //bits = 134
+    //bytes = 17
+    //e = +108 = (108 + 1) % 9 = 1
 
 
-//		1.321974894 648943195 489415948 943195489   156
-//		- --------- --------- --------- ---------   ---
-//index	5		4			3		2			1	  0
-//bit	4		30		    30		30			30	 10
-//bits = 134
-//bytes = 17
-//e = 0
+    //		1.321974894 648943195 489415948 943195489   156
+    //		- --------- --------- --------- ---------   ---
+    //index	5		4			3		2			1	  0
+    //bit	4		30		    30		30			30	 10
+    //bits = 134
+    //bytes = 17
+    //e = 0 = 1 % 0 = 1
 
-//Data =>
-//3     EndDigitsCount
-//5-61  Exponent
+    //		132 197489464 894319548 941594894 319548915 6
+    //		--- --------- --------- --------- --------- -
+    //index	5       4		3			2       1	    0
+    //bit	4		30		    30		30			30	 10
+    //bits = 134
+    //bytes = 17
+    //e = -7 = (-7 + 1) % 9 = 3
 
-//Number=>
-//NumberBytes
-//Last 1 Sign
+    //		13219748 946489431 954894159 489431954 89156
+    //		-------- --------- --------- --------- -----
+    //index	4		    3           2	       1      0
+    //bit	    27		30	    30		    30		 17
+    //bits = 134
+    //bytes = 17
+    //e = -100 = (-100 + 1) % 9 = 0
+
+    //Data =>
+    //3     EndDigitsCount
+    //5-61  Exponent
+
+    //Number=>
+    //NumberBytes
+    //Last 1 Sign
     public class ArNumber
     {
         private byte[] _Data;
@@ -103,6 +127,7 @@ namespace Aritiafel.Organizations.RaeriharUniversity
         public ArNumber()
         {
             _Data = new byte[1];
+            _Numbers = new byte[100];
         }
 
         public ArNumber(ArNumber a)
@@ -145,8 +170,28 @@ namespace Aritiafel.Organizations.RaeriharUniversity
         public void SetNumberBlock(int index, int value)
         {
             //參考E
-            _Numbers = new byte[30];
+            //_Numbers = new byte[30];
+            if(index == 0)
+            {
+                switch(value.ToString().Length)
+                {
+                    case 0:
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                    case 5:
+                    case 6:
+                    case 7:
+                    case 8:
+                }
+            }
+            //if(!last)
+            //{
+                
+            //}
 
+            
             //_Numbers = new byte[((s.Length - 1) / 9 * 15 + 18) / 4];
         }
 
